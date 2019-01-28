8785b72f627893aef2cd6cc3d5c07f540b5c206d
babril <beatriz92abril@gmail.com>
2019-01-28 11:59:01 +0100

    commit1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'Esto es un programa de prueba GIT';
+        echo 'Primera línea que escribo, tras inicializar GIT';
+        echo 'Voy a guardar el primer COMMIT';
+        
+        ?>
+    </body>
+</html>
